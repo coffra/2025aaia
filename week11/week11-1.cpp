fec48674fcea7a17a0f20ad57d8f891fa3425f8c
coffra <wanghaba25@gmail.com>
2025-05-02 11:53:56 +0800

    week11

---
+++ week11/week11-1.cpp
@@ -0,0 +1,17 @@
+///week11-1.cpp
+#include <stdio.h>
+
+int addnum(int a,int b)
+{
+    printf("在函式addnum()裡，得到參數a:%d b:%d\n",a,b);
+    int ans=a+b;
+    printf("算出答案%d要return出去喔\n",ans);
+    return ans;
+}
+
+int main()
+{
+    printf("在main()呼叫使用 addnum()\n");
+    int ans=addnum(2,3);
+    printf("在main() 得到答案%d\n",ans);
+}
